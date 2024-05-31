74f0b443d018e18a08d989c34ec33ab9654bf64a
mrmoh <mohd7@iastate.edu>
2024-05-31 05:00:13 -0500

    function more understanding

--- Basics.py
+++ Basics.py
@@ -2,13 +2,14 @@ x = 10
 print("helo")
 name = "broiler kozhi"
 
-def chicken_macha(name, x)
+def chicken_macha(name, x):
     print("yo my name's chicken")
     y = 3
     x = 5
-    return name, x
+    return "hello", 25
 
 print("aliyaa")
 aliyaa_name, numbr = chicken_macha(name, x)
 machaa = "then we get" + aliyaa_name + "and", str(numbr)
 print(machaa)
+print(chicken_macha(name, x), "this is my first try")
