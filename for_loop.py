32f52de62d91734bfddc6d6a65bcb99642e5fb5c
mrmoh <mohd7@iastate.edu>
2024-06-02 00:51:57 -0500

    new loop concepts

---
+++ for_loop.py
@@ -0,0 +1,12 @@
+for i in [5, 4, 3, 2, 1] :
+    print(i)
+print("blast off!")
+for j in range(5) :
+    print(j)
+print("blast off!")
+chick = ["a","macha",8]
+for chi in chick:
+    print(chi)
+print("end")
+greet = "  my name is chciken bob "
+print(greet.strip())
