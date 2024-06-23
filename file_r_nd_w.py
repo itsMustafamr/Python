bb7fb4334436643c015eca697d6147618ec65d25
itsmustafamr <mr.mohdmustafa007@gmail.com>
2024-06-23 12:29:45 -0500

    Tried split fn with email start

--- file_r_nd_w.py
+++ file_r_nd_w.py
@@ -7,6 +7,13 @@ print("count =", c)
 fleg = open('water.txt')
 for a in fleg:
     print("chicken")
-quit()
+#quit()
 print("ball")
 
+ftake = open('email.txt')
+for i in ftake:
+    i = i.rstrip()
+    if not i.startswith('From ') : continue
+    word = i.split()
+    print(word[2])
+    print(word)
