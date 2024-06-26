f3d31fbaabcc054476bd79081097e8ca6e293446
mrmoh <mohd7@iastate.edu>
2024-06-26 16:22:33 -0500

    How constructor works and how vairables before the constructor get's executed

--- party1.py
+++ party1.py
@@ -1,13 +1,16 @@
 class PartyAnimal:
    x = 0
-#    def __init__(self):
-#      self.x = 0
+   print("i will be constructed")
+
+   def __init__(self):
+    self.x = 0
+    print("i was constructed")
 
    def party(self) :
      self.x = self.x + 1
      print("So far",self.x)
 
-an = PartyAnimal()
+an = PartyAnimal() #
 an.party()
 an.party()
 an.party()
