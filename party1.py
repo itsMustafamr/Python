7460195f7679c84b0d9c46cad1161ebddf9c05cf
mrmoh <mohd7@iastate.edu>
2024-06-26 15:17:35 -0500

    Basic understanding of objects and use in full class

---
+++ party1.py
@@ -0,0 +1,14 @@
+class PartyAnimal:
+   x = 0
+#    def __init__(self):
+#      self.x = 0
+
+   def party(self) :
+     self.x = self.x + 1
+     print("So far",self.x)
+
+an = PartyAnimal()
+an.party()
+an.party()
+an.party()
+print("dir is ", dir(an))
